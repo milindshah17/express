3509658736f725d8660b99c09c2fea3be6d00a73
Milind Shah <shahmilind07@gmail.com>
2021-10-21 22:09:03 +0530

    21-10

--- src/events.js
+++ src/events.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const { pid } = require('process');
+//const { pid } = require('process');
+
 
 function createRouter(db) {
   const router = express.Router();
-  const owner = '';
+
+  router.use(express.urlencoded({extended: true}));
+  router.use(express.json())
+
+  
+//  const owner = '';
 
   // the routes are defined here
 
@@ -38,6 +44,8 @@ function createRouter(db) {
   });
 
 
+
+
   router.get('/product', function (req, res, next) {
     var querypm = req.query;
     var pid=parseInt(querypm.product_id);
@@ -57,9 +65,38 @@ function createRouter(db) {
 
 
 
+  router.get('/products', function (req, res, next) {
+    var querypm = req.query;
+    var cat= querypm.category;
+    var dist=querypm.district;
+    db.query(
+      "SELECT * from product Inner Join Seller on product.seller_id=seller.seller_id where Product_category =  '" +cat+ "'",
+      
+      (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json(results);
+        }
+      }
+    );
+  });
+
+
+
   
+
+  router.post('/test', function(req, resp){
+    var querypm = JSON.parse(JSON.stringify(req.body));
+    var name= querypm.name;
+    console.log(name);      // your JSON
+    resp.status(200).json(name);    // echo the result back
+  });
 
   return router;
 }
+
+
 
 module.exports = createRouter;
