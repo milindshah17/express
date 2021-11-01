c7d2c69d556b0b50a86825d1573c1be7fb93f8d3
Milind Shah <shahmilind07@gmail.com>
2021-11-01 15:44:12 +0530

    01-11-2021

--- src/events.js
+++ src/events.js
@@ -30,7 +30,7 @@ function createRouter(db) {
 
   router.get('/products', function (req, res, next) {
     db.query(
-      'SELECT * from product ',
+      'SELECT * from product Inner Join Seller on product.seller_id=seller.seller_id where product.status="ongoing"',
       
       (error, results) => {
         if (error) {
@@ -165,6 +165,33 @@ function createRouter(db) {
   });
 
 
+  router.post('/slregister', function (req, res) {
+    var querypm = JSON.parse(JSON.stringify(req.body));
+    var name= querypm.name;
+    var phone= querypm.phone;
+    var email= querypm.email;
+    var username= querypm.username;
+    var password= querypm.password;
+    var state= querypm.state;
+    var district= querypm.district;
+    var address= querypm.address;
+    var aadhar=querypm.aadhar;
+    console.log(querypm)
+    db.query(
+      "insert into seller (seller_username,seller_name,seller_phone,seller_email,seller_aadhar,seller_address,seller_district,seller_state,seller_password) values ('"+username+"','"+name+"','"+phone+"','"+email+"','"+aadhar+"','"+address+"','"+district+"','"+state+"','"+password+"') ",
+      
+      (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json(results);
+        }
+      }
+    );
+  });
+
+
 
   router.post('/bid', function (req, res) {
     var querypm = JSON.parse(JSON.stringify(req.body));
@@ -198,7 +225,75 @@ function createRouter(db) {
     var seller_id = parseInt(querypm.sid);
     console.log (seller_id);
      db.query(
-      "select * from product where Seller_id ="+seller_id+" " ,
+      "select * from product Inner Join Seller on product.seller_id=seller.seller_id where product.Seller_id ="+seller_id+" and status ='ongoing' " ,
+        (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json(results);
+        }
+      }
+    );
+  });
+  
+  router.post('/sltcns', function (req, res) {
+    var querypm = JSON.parse(JSON.stringify(req.body));
+    var seller_id = parseInt(querypm.sid);
+    console.log (seller_id);
+     db.query(
+      "SELECT  *  FROM  product  INNER JOIN      bid ON product.bid_id = bid.bid_id  INNER JOIN      buyer ON bid.buyer_id = buyer.buyer_id    INNER JOIN     seller ON product.Seller_id = seller.Seller_id       where product.Seller_id ="+seller_id+" and status='sold' ",
+        (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json(results);
+        }
+      }
+    );
+  });
+
+  router.post('/bytcns', function (req, res) {
+    var querypm = JSON.parse(JSON.stringify(req.body));
+    var buyer_id = parseInt(querypm.sid);
+    console.log(buyer_id);
+     db.query(
+      "SELECT  *  FROM  product  INNER JOIN      bid ON product.bid_id = bid.bid_id  INNER JOIN      buyer ON bid.buyer_id = buyer.buyer_id    INNER JOIN     seller ON product.Seller_id = seller.Seller_id       where bid.buyer_id ="+buyer_id+" and status='sold' " ,
+        (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json(results);
+        }
+      }
+    );
+  });
+
+  router.post('/bids', function (req, res) {
+    var querypm = JSON.parse(JSON.stringify(req.body));
+    var buyer_id = parseInt(querypm.b_id);
+    console.log(buyer_id);
+     db.query(
+      "select * from bid Inner Join product on bid.product_id=product.product_id where bid.buyer_id ="+buyer_id+" " ,
+        (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json(results);
+        }
+      }
+    );
+  });
+
+  router.post('/slviewunsold', function (req, res) {
+    var querypm = JSON.parse(JSON.stringify(req.body));
+    var seller_id = parseInt(querypm.sid);
+    console.log (seller_id);
+     db.query(
+      "select * from product Inner Join Seller on product.seller_id=seller.seller_id where product.Seller_id ="+seller_id+" and status ='unsold' " ,
         (error, results) => {
         if (error) {
           console.log(error);
@@ -211,6 +306,46 @@ function createRouter(db) {
   });
   
 
+  /*router.get('/filters', function (req, res, next) {
+    var querypm = req.query;
+    var price =parseInt(querypm.price);
+    var keyword=querypm.keyword;
+    var state = querypm.state;
+    var district = querypm.district;
+    var category = querypm.category;
+
+    if(price != ''){
+      query+='and '
+    }
+
+    var pid=parseInt(querypm.product_id);
+    const baseURL = 'select * from product';
+    if(typeof prm1 != 'undefined')
+    
+
+    const fullquery = baseURL;
+    console.log({ fullURL });
+    
+    db.query(
+      "SELECT * from product",
+      
+      (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json(results);
+        }
+      }
+    );
+  });*/
+    
+
+  
+   
+
+
+
   router.post('/test', function(req, res){
     var querypm = JSON.parse(JSON.stringify(req.body));
     var name= querypm.name;
@@ -219,9 +354,51 @@ function createRouter(db) {
     res.status(200).json(name);    // echo the result back
   });
 
+
+  router.get('/category', function (req, res, next) {
+    var querypm = req.query;
+    var cat=querypm.category;
+    db.query(
+      "SELECT * from product Inner Join Seller on product.seller_id=seller.seller_id where Product_category =  '" + cat + "'",
+      
+      (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json(results);
+        }
+      }
+    );
+  });
+
+
+
+
+ const cron = require("node-cron");
+  cron.schedule(" */1 * * * *", function() {
+    console.log("running a task every minute");
+    db.query(
+      "update product set status = 'sold' where product_endtime < now() and current_bid is not null and status!='sold'; update product set status = 'unsold' where product_endtime < now() and current_bid is null and status!='unsold'; ",
+      function (err, result,) {
+      if (err) throw err;
+      console.log(result);
+    });
+  }); 
+
+  /*setInterval(function(){ 
+    
+    console.log("hi")
+    db.query(" SELECT seller_name FROM seller where seller_id=100 ", function (err, result,) {
+      if (err) throw err;
+      console.log(result);
+    });
+
+  },20000)*/
+
+
   return router;
 }
-
 
 
 module.exports = createRouter;
